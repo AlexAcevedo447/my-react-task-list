994cfab3188e6e1407959d309ffe2faffcef5036
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2023-08-21 18:11:02 -0500

    last points

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Heading, Text } from "@chakra-ui/react";
 import { useNsColorValues } from "./hooks";
-
+import { TasksAPI } from "../services/task_api";
+console.log(TasksAPI(this))
 export const Header = () => {
     const { fontColor } = useNsColorValues()
     return (
--- src/services/task_api.js
+++ src/services/task_api.js
@@ -1,26 +1,31 @@
 import axios from 'axios';
-export class TaskAPI {
-    static instance;
-    #token;
-    #baseurl;
-    constructor() {
-        this.#token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiZW1haWwiOiJwZXBpdG9AcGVyZXouY29tIiwidXNlcm5hbWUiOiJQZXBpdG9QZXJlejEyMyIsInBhc3N3b3JkIjoiJDJhJDA4JERSU2hTYUx3cGpJVFpDLzJReE1aQ2V6UkQyV2d3YzVNTDdaSVJpZUUxZTRrYXAzV2pVRy82IiwiaWF0IjoxNjg5ODEyNjIzfQ.XW6tqsVlj9dI6SNoiT9X_KxoT2eEA-999851MCeH5j8`;
-        this.#baseurl = `http://127.0.0.1:3200`
-        if (!TaskAPI.instance) TaskAPI.instance = this;
-        else return TaskAPI.instance;
-        console.log(TaskAPI.instance)
-    }
-    async getTasks() {
-        try {
-            const response = await axios.get(`${this.#baseurl}/tasks`, {
-                headers: {
-                    "Authorization": this.#token
-                }
-            });
-            return response;
-        } catch (error) {
-            console.log(error);
-            return null;
+export function TasksAPI(obj) {
+    let service = class {
+        static instance;
+        #token;
+        #baseurl;
+        constructor() {
+            this.#token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiZW1haWwiOiJwZXBpdG9AcGVyZXouY29tIiwidXNlcm5hbWUiOiJQZXBpdG9QZXJlejEyMyIsInBhc3N3b3JkIjoiJDJhJDA4JERSU2hTYUx3cGpJVFpDLzJReE1aQ2V6UkQyV2d3YzVNTDdaSVJpZUUxZTRrYXAzV2pVRy82IiwiaWF0IjoxNjg5ODEyNjIzfQ.XW6tqsVlj9dI6SNoiT9X_KxoT2eEA-999851MCeH5j8`;
+            this.#baseurl = `http://127.0.0.1:3200`
+            if (!service.instance) service.instance = this;
+            else return service.instance;
+            console.log(service.instance)
+        }
+        async getTasks() {
+            try {
+                const response = await axios.get(`${this.#baseurl}/tasks`, {
+                    headers: {
+                        "Authorization": this.#token
+                    }
+                });
+                return response;
+            } catch (error) {
+                console.log(error);
+                return null;
+            }
         }
     }
+    console.log(TasksAPI)
+    if (!obj instanceof Window || !obj instanceof Document || typeof obj !== 'undefined') return service;
+    else return {};
 }
